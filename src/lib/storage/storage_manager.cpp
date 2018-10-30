386cce307d2cfe2fe91181ca8a7ea57e0ab437b6
Alexander Riese <alexander.riese@student.hpi.de>
2018-10-30 14:46:14 +0100

    finished step 6

--- src/lib/storage/storage_manager.cpp
+++ src/lib/storage/storage_manager.cpp
@@ -10,38 +10,53 @@
 namespace opossum {
 
 StorageManager& StorageManager::get() {
-  return *(new StorageManager());
-  // A really hacky fix to get the tests to run - replace this with your implementation
+  static StorageManager instance;
+  return instance;
 }
 
 void StorageManager::add_table(const std::string& name, std::shared_ptr<Table> table) {
-  // Implementation goes here
+  if (has_table(name)) {
+    throw std::runtime_error("this table name is already used: " + name);
+  }
+  _tables[name] = table;
 }
 
 void StorageManager::drop_table(const std::string& name) {
-  // Implementation goes here
+  if (!has_table(name)) {
+    throw std::runtime_error("this table name does not exist: " + name);
+  }
+  _tables.erase(name);
 }
 
 std::shared_ptr<Table> StorageManager::get_table(const std::string& name) const {
-  // Implementation goes here
-  return nullptr;
+  DebugAssert(has_table(name), "this table name does not exist: " + name);
+  return _tables.at(name);
 }
 
 bool StorageManager::has_table(const std::string& name) const {
-  // Implementation goes here
-  return false;
+  return _tables.find(name) != _tables.end();
 }
 
 std::vector<std::string> StorageManager::table_names() const {
-  throw std::runtime_error("Implement StorageManager::table_names");
+  auto table_names = std::vector<std::string>(_tables.size());
+  std::transform(_tables.begin(), _tables.end(), table_names.begin(), [](auto value_pair){
+    return value_pair.first;
+  });
+  return table_names;
 }
 
 void StorageManager::print(std::ostream& out) const {
-  // Implementation goes here
+  for (auto &table_pair : _tables) {
+    auto table_name = table_pair.first;
+    auto column_count = table_pair.second->column_count();
+    auto row_count = table_pair.second->row_count();
+    auto chunk_count = table_pair.second->chunk_count();
+    out << table_name << ' ' << column_count << ' ' << row_count << ' ' << chunk_count << std::endl;
+  }
 }
 
 void StorageManager::reset() {
-  // Implementation goes here;
+  _tables.clear();
 }
 
 }  // namespace opossum
--- src/lib/storage/storage_manager.hpp
+++ src/lib/storage/storage_manager.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -44,6 +44,8 @@ class StorageManager : private Noncopyable {
   StorageManager() {}
   StorageManager& operator=(StorageManager&&) = default;
 
-  // Implementation goes here
+  // mapping from table names to table object pointers
+  std::unordered_map<std::string, std::shared_ptr<Table>> _tables;
+
 };
 }  // namespace opossum
