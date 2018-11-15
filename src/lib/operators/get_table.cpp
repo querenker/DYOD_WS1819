00d97023afb4769c252098b68975f08993e2593a
Alexander Riese <alexander.riese@student.hpi.de>
2018-11-15 15:24:17 +0100

    fix #26

---
+++ src/lib/operators/get_table.cpp
@@ -0,0 +1,16 @@
+#include "get_table.hpp"
+#include "../storage/storage_manager.hpp"
+
+namespace opossum {
+
+GetTable::GetTable(const std::string& name) : _table_name(name) {}
+
+std::shared_ptr<const Table> GetTable::_on_execute() {
+    return StorageManager::get().get_table(_table_name);
+}
+
+const std::string& GetTable::table_name() const {
+    return _table_name;
+}
+
+} // namespace opossum
